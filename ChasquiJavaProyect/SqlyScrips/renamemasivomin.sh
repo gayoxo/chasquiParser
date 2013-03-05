2041a47bdce5e69d20aad9e8b17289eb51856249
Joaquin Gayoso Cabada <gayoxo@gmail.com>
2013-03-05 17:27:41 +0100

    20130305

---
+++ ChasquiJavaProyect/SqlyScrips/renamemasivomin.sh
@@ -0,0 +1,10 @@
+find -depth |
+while read SRC; 
+do
+	echo $SRC
+    DST=`dirname "${SRC}"`/`basename "${SRC}" | tr '[A-Z]' '[a-z]'`
+    if [ "${SRC}" != "${DST}" ]
+    then
+        [ ! -e "${DST}" ] && mv -T "${SRC}" "${DST}" || echo "${SRC} was not renamed"
+    fi
+done
